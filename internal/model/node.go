27b038fe924efd2a5820dc5df0db7451222e385d
Hietan <yamasaki.kazuma@hietan.com>
2025-01-19 15:08:48 +0900

    [ADD] Add Struct "Node"

---
+++ internal/model/node.go
@@ -0,0 +1,30 @@
+package model
+
+type Node[T any] struct {
+	Label    Label[T]
+	Parent   *Node[T]
+	Children []*Node[T]
+}
+
+func NewNode[T any](label Label[T]) *Node[T] {
+	return &Node[T]{
+		Label:    label,
+		Parent:   nil,
+		Children: nil,
+	}
+}
+
+func (n *Node[T]) AddChild(child *Node[T]) {
+	child.Parent = n
+	n.Children = append(n.Children, child)
+}
+
+func (n *Node[T]) RemoveChild(child *Node[T]) {
+	for i, c := range n.Children {
+		if c == child {
+			n.Children = append(n.Children[:i], n.Children[i+1:]...)
+			child.Parent = nil
+			break
+		}
+	}
+}
