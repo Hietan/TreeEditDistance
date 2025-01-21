62f8e444793a48f28712c0db9565aa286793251a
Hietan <hietan622@gmail.com>
2025-01-21 10:33:31 +0900

    [ADD] Implement first two loops

--- internal/solver/solver.go
+++ internal/solver/solver.go
@@ -6,6 +6,12 @@ import (
 )
 
 func CalcEditDistance[T any](t1, t2 *model.Tree[T]) int {
-	fmt.Println(t1.Size(), t2.Size())
-	return 0
+	t1Size := t1.Size()
+	t2Size := t2.Size()
+
+	for i := 1; i <= t1Size; i++ {
+		for j := 1; j <= t2Size; j++ {
+
+		}
+	}
 }
