70ee1e880e90ead420837c27115acfb037885f23
Hietan <hietan622@gmail.com>
2025-02-09 08:58:47 +0900

    [ADD] Implement type Node for solver

--- internal/solver/node.go
+++ internal/solver/node.go
@@ -3,18 +3,16 @@ package solver
 import "fmt"
 
 type Node[T any] struct {
-	id       int
 	value    T
-	parent   *int
+	parent   int
 	children []int
 }
 
-func NewNode[T any](id int, value T) *Node[T] {
+func NewNode[T any](value T) *Node[T] {
 	return &Node[T]{
-		id:       id,
 		value:    value,
-		parent:   nil,
-		children: nil,
+		parent:   -1,
+		children: []int{},
 	}
 }
 
@@ -22,53 +20,31 @@ func (n *Node[T]) String() string {
 	return fmt.Sprintf("%v", n.value)
 }
 
-func (n *Node[T]) GetId() int {
-	return *n.id
-}
-
-func (n *Node[T]) SetId(id int) {
-	if id <= 0 {
-		panic("Node's ID must be positive")
-	}
-	n.id = &id
-}
-
 func (n *Node[T]) GetValue() T {
 	return n.value
 }
 
-func (n *Node[T]) GetParent() *Node[T] {
+func (n *Node[T]) GetParent() int {
 	return n.parent
 }
 
-func (n *Node[T]) SetParent(parent *Node[T]) {
+func (n *Node[T]) SetParent(parent int) {
 	n.parent = parent
 }
 
-func (n *Node[T]) GetChildren() []*Node[T] {
+func (n *Node[T]) GetChildren() []int {
 	return n.children
 }
 
-func (n *Node[T]) AddChild(child *Node[T]) {
-	child.SetParent(n)
+func (n *Node[T]) AddChild(child int) {
 	n.children = append(n.children, child)
 }
 
-func (n *Node[T]) RemoveChild(child *Node[T]) {
+func (n *Node[T]) RemoveChild(child int) {
 	for i, c := range n.children {
 		if c == child {
 			n.children = append(n.children[:i], n.children[i+1:]...)
-			child.SetParent(nil)
 			break
 		}
 	}
-}
-
-func parentsIncludeMyself[T any](n *Node[T]) []*Node[T] {
-	var parents []*Node[T]
-	parents = append(parents, n)
-	for p := n.GetParent(); p != nil; p = p.GetParent() {
-		parents = append(parents, p)
-	}
-	return parents
 }
